e0f83696e0881b78c5b81353f381b4dfa714df58
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-09-30 23:04:47 +0100

    add initial process scripts to perform tai ascii animation on a flip trick asset

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-console.log('hello')
+#!/usr/bin/env node
+
+import { spawn } from 'child_process'
+
+const scriptPath = './rust-binaries/target/release/tai';
+const args = ['-c', '-d', '--scale', '1', '--sleep', '50', './assets/switch-heel-body-varial.gif'];
+const taiProcess = spawn(scriptPath, args);
+
+// Listen for stdout data (ASCII art output)
+const onData = (data: any) => {
+    process.stdout.write(data);
+};
+
+// Attach the event listener
+taiProcess.stdout.on('data', onData);
+
+setTimeout(() => {
+    taiProcess.stdout.off('data', onData);
+    console.log('Stopped listening for data.');
+    // Ensure the process is killed on script exit
+    taiProcess.kill('SIGINT');
+}, 30000);  
+
+taiProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+taiProcess.on('close', (code) => {
+    console.log(`Child process exited with code ${code}`);
+});
+
+
+
+
