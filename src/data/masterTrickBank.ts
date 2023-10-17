4bc59b80e07b5d79820314967b44cd766b0b4427
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-10-17 16:54:46 +0100

    add fakie kickflip gif

--- src/data/masterTrickBank.ts
+++ src/data/masterTrickBank.ts
@@ -6,48 +6,53 @@ const masterTrickBank: TrickBank = {
   hardflip: {
     name: 'Hardflip',
     propName: 'hardflip',
-    filepath: `${tricksBasePath}hardflip.gif`
+    filepath: `${tricksBasePath}hardflip.gif`,
   },
   nollieDoubleKickflip: {
     name: 'Nollie Double Kickflip',
     propName: 'nollieDoubleKickflip',
-    filepath: `${tricksBasePath}nollie-double-kickflip-large.gif`
+    filepath: `${tricksBasePath}nollie-double-kickflip-large.gif`,
   },
   switchHeelflipBodyVarial: {
     name: 'Switch Heelflip Body Varial',
     propName: 'switchHeelflipBodyVarial',
-    filepath: `${tricksBasePath}switch-heel-body-varial.gif`
+    filepath: `${tricksBasePath}switch-heel-body-varial.gif`,
   },
   varialHeelflip: {
     name: 'Varial Heelflip',
     propName: 'varialHeelflip',
-    filepath: `${tricksBasePath}varial-heelflip.gif`
+    filepath: `${tricksBasePath}varial-heelflip.gif`,
   },
   kickflip: {
     name: 'Kickflip',
     propName: 'kickflip',
-    filepath: `${tricksBasePath}kickflip.gif`
+    filepath: `${tricksBasePath}kickflip.gif`,
   },
   lateHardflip: {
     name: 'Late Hardflip',
     propName: 'lateHardflip',
-    filepath: `${tricksBasePath}jamie/late-hardflip.gif`
+    filepath: `${tricksBasePath}jamie/late-hardflip.gif`,
   },
   fakieDoubleDolphinFlip: {
     name: 'Fakie Double Dolphin Flip',
     propName: 'fakieDoubleDolphinFlip',
-    filepath: `${tricksBasePath}jamie/fakie-double-dolphin-flip.gif`
+    filepath: `${tricksBasePath}jamie/fakie-double-dolphin-flip.gif`,
   },
   doubleHardflip: {
     name: 'Double Hardflip',
     propName: 'doubleHardflip',
-    filepath: `${tricksBasePath}jamie/double-hardflip.gif`
+    filepath: `${tricksBasePath}jamie/double-hardflip.gif`,
   },
   doubleHeelflipSigma: {
     name: 'Double Heelflip Sigma',
     propName: 'doubleHeelflipSigma',
-    filepath: `${tricksBasePath}jamie/double-heelflip-sigma.gif`
-  }
+    filepath: `${tricksBasePath}jamie/double-heelflip-sigma.gif`,
+  },
+  fakieKickflip: {
+    name: 'Fakie Kickflip',
+    propName: 'fakieKickflip',
+    filepath: `${tricksBasePath}fakieKickflip.gif`,
+  },
 };
 
 export default masterTrickBank;
