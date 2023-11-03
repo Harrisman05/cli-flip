a575c431f640960f0af251eaeb1d36aeac74dd4d
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-11-03 20:27:04 +0000

    remove all single line \n statements and add them to end of previous lines

--- src/utils/process/writeAnswerPromptReplica.ts
+++ src/utils/process/writeAnswerPromptReplica.ts
@@ -2,19 +2,18 @@ import chalk from 'chalk';
 import { score } from '../../model/Score';
 
 const writeAnswerPromptReplica = (choices: string[]): void => {
-  process.stdout.write(`${chalk.green('?')} ${chalk.bold('Guess the trick! 🛹🤔')} ${chalk.dim('(input blocked)')}\n`);
-  process.stdout.write('\n');
+  process.stdout.write(
+    `${chalk.green('?')} ${chalk.bold('Guess the trick! 🛹🤔')} ${chalk.dim('(input blocked)')}\n\n`,
+  );
   process.stdout.write(`  ${chalk.yellow(choices[0])}\n`);
   process.stdout.write(`  ${chalk.yellow(choices[1])}\n`);
   process.stdout.write(`  ${chalk.yellow(choices[2])}\n`);
-  process.stdout.write(`  ${chalk.yellow(choices[3])}\n`);
-  process.stdout.write('\n');
+  process.stdout.write(`  ${chalk.yellow(choices[3])}\n\n`);
   process.stdout.write(
     `  ✅ - ${chalk.green(score.correctAnswers)} | ❌ - ${chalk.red(score.incorrectAnswers)} | ${
       score.currentQuestion
-    }/${score.totalQuestions}`,
+    }/${score.totalQuestions}\n`,
   );
-  process.stdout.write('\n');
 };
 
 export default writeAnswerPromptReplica;
