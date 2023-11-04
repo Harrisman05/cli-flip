cdb7104306d3d47400da9dad713fd198f4b8bec0
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-11-04 10:17:25 +0000

    refactor callback to be more readable and make tests easier to understand

--- src/utils/process/closeGifProcess.ts
+++ src/utils/process/closeGifProcess.ts
@@ -3,11 +3,13 @@ import { Tricks } from '../../model/Tricks';
 import removePlayingStatusText from '../cleanTerminal/removePlayingStatusText';
 import guessGif from '../quizControl/guessGif';
 
-const closeGifProcess = (currentQuizTricks: Tricks, correctTrick: Trick, currentChoices: string[]) => {
-  return (): void => {
+const closeGifProcess = (currentQuizTricks: Tricks, correctTrick: Trick, currentChoices: string[]): (() => void) => {
+  const closeCallback = (): void => {
     removePlayingStatusText();
     guessGif(currentQuizTricks, correctTrick, currentChoices);
   };
+
+  return closeCallback;
 };
 
 export default closeGifProcess;
