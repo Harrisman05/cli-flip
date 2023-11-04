ab6351a1d339ebfab7d24d6f70fbbc3a9733abf5
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-11-04 10:53:54 +0000

    add removePlayingStatusText tests

---
+++ tests/src/utils/cleanTerminal/removePlayingStatusText.test.ts
@@ -0,0 +1,25 @@
+import removePlayingStatusText from '../../../../src/utils/cleanTerminal/removePlayingStatusText';
+import readline from 'readline';
+
+const spyReadlineClearLine = jest.spyOn(readline, 'clearLine');
+const spyReadlineMoveCursor = jest.spyOn(readline, 'moveCursor');
+
+describe('removePlayingStatusText test suites', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should call readline.clearLine once with correct parameters', () => {
+    removePlayingStatusText();
+
+    expect(spyReadlineClearLine).toHaveBeenCalledTimes(1);
+    expect(spyReadlineClearLine).toHaveBeenCalledWith(process.stdout, 0);
+  });
+
+  it('should call readline.moveCursor once with correct parameters', () => {
+    removePlayingStatusText();
+
+    expect(spyReadlineMoveCursor).toHaveBeenCalledTimes(1);
+    expect(spyReadlineMoveCursor).toHaveBeenCalledWith(process.stdout, 0, 3);
+  });
+});
