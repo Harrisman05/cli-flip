907fce29e4ccc961ddf90b11ffb262bf68ef36e0
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-11-15 22:47:20 +0000

    add env file for outputting gif

--- src/outputBrailleGif/outputBrailleGif.ts
+++ src/outputBrailleGif/outputBrailleGif.ts
@@ -4,7 +4,7 @@ import getTrickWithStanceFileNames from './fs/getTrickWithStanceFilesNames';
 import outputFramesSequentially from './fs/outputFramesSequentially';
 
 const outputBrailleGif = async (): Promise<void> => {
-  const trickWithStancePath = 'output/fakie-double-dolphin-flip.gif';
+  const trickWithStancePath = `output/${process.env.INPUT_GIF}`;
   const frameNames = await getTrickWithStanceFileNames(trickWithStancePath);
   outputFramesSequentially(frameNames);
 };
